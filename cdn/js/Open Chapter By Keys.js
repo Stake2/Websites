b43126952587bc704d531cdf9fc13c975d0b05ca
stake2 <kiko12345kiko@hotmail.com>
2020-09-24 16:33:02 -0300

    Fixed text string in the OpenChapterByKeys.js script, removing a space

--- cdn/js/Open Chapter By Keys.js
+++ cdn/js/Open Chapter By Keys.js
@@ -8,7 +8,7 @@ document.addEventListener("keyup", function() {
 		if (is_ctrl === true || is_ctrl === false) {
 			if (event.keyCode === 37) {
 				if (ChapterNumber != 1) {
-					var opening_chapter_text = "Opening chapter number " + '"' + (ChapterNumber - 1) + " - " + ChapterTitle + '"' + " " + "...";
+					var opening_chapter_text = "Opening chapter number " + '"' + (ChapterNumber - 1) + " - " + ChapterTitle + '"' + "...";
 
 					console.log(opening_chapter_text);
 					openCity("chapter-" + (ChapterNumber - 1));
@@ -18,7 +18,7 @@ document.addEventListener("keyup", function() {
 
 			if (event.keyCode === 39) {
 				if (ChapterNumber != Last_Chapter) {
-					var opening_chapter_text = "Opening chapter number " + '"' + (ChapterNumber - 1) + " - " + ChapterTitle + '"' + " " + "...";
+					var opening_chapter_text = "Opening chapter number " + '"' + (ChapterNumber - 1) + " - " + ChapterTitle + '"' + "...";
 
 					console.log(opening_chapter_text);
 					openCity("chapter-" + (ChapterNumber + 1));
