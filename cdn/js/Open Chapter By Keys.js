ff09b8018d8f934adfdf7d23f770a48cb87b14d1
stake2 <kiko12345kiko@hotmail.com>
2020-10-09 13:19:13 -0300

    Removed free key feature from the OpenChapterByKeys.js file

--- cdn/js/Open Chapter By Keys.js
+++ cdn/js/Open Chapter By Keys.js
@@ -1,12 +1,14 @@
 document.addEventListener("keyup", function() {
 	var is_ctrl;
+	var is_shift;
 	var opening_chapter_text;
 
 	if (event.keyCode === 37 || event.keyCode === 39) {
 		event.preventDefault();
 		is_ctrl = !!event.ctrlKey;
+		is_shift = !!event.shiftKey;
 
-		if (is_ctrl === true || is_ctrl === false) {
+		if (is_ctrl === true || is_shift === true) {
 			if (event.keyCode === 37) {
 				if (ChapterNumber != 1) {
 					opening_chapter_text = "Opening chapter number " + '"' + (ChapterNumber - 1) + "...";
