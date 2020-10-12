d25c95cc0284473dafd3977ee3805e6cc55e1da8
stake2 <kiko12345kiko@hotmail.com>
2020-10-11 23:41:20 -0300

    Modified the css and js files of the computer and mobile button bars

--- cdn/js/ShowHide.js
+++ cdn/js/ShowHide.js
@@ -1,16 +1,19 @@
+var computer_buttons_element = "computer_buttons_bar";
+var show_computer_buttons_element = "show_computer_buttons";
+
 function Hide_Computer_Buttons() {
-	document.getElementById("computer_buttons_bar").style.display = "none";
-	document.getElementById("show_Computer_Menu").style.display = "block";
-	document.getElementById("computer_buttons_bar").className += 'w3-animate-top';
-	document.getElementById("computer_buttons_bar").className += ' w3-animate-zoom';
+	document.getElementById(computer_buttons_element).style.display = "none";
+	document.getElementById(show_computer_buttons_element).style.display = "block";
+	document.getElementById(computer_buttons_element).className += 'w3-animate-top';
+	document.getElementById(computer_buttons_element).className += ' w3-animate-zoom';
 }
 
 function Show_Computer_Buttons() {
-	document.getElementById("computer_buttons_bar").style.display = "block";
-	document.getElementById("show_Computer_Menu").style.display = "none";
-	var element = document.getElementById("computer_buttons_bar");
+	document.getElementById(computer_buttons_element).style.display = "block";
+	document.getElementById(show_computer_buttons_element).style.display = "none";
+	var element = document.getElementById(computer_buttons_element);
 	element.className = element.className.replace("w3-animate-top", "");
 	element.className = element.className.replace(" w3-animate-zoom", "");
 }
 
-console.log("ShowHide Script was loaded.");
+console.log("Show Hide Script was loaded.");
--- cdn/js/sidemenu.js
+++ cdn/js/sidemenu.js
@@ -1,11 +1,14 @@
-function openNav() {
-	document.getElementById("mobile_button_sidebar").style.width = "300px";
-	document.getElementById("ShowMenu").style.display = "none";
+var mobile_buttons_element = "mobile_button_sidebar";
+var show_mobile_buttons_element = "show_mobile_buttons";
+
+function Show_Mobile_Buttons() {
+	document.getElementById(mobile_buttons_element).style.width = "300px";
+	document.getElementById(show_mobile_buttons_element).style.display = "none";
 }
 
-function closeNav() {
-	document.getElementById("mobile_button_sidebar").style.width = "0px";
-	document.getElementById("ShowMenu").style.display = "block";
+function Hide_Mobile_Buttons() {
+	document.getElementById(mobile_buttons_element).style.width = "0px";
+	document.getElementById(show_mobile_buttons_element).style.display = "block";
 }
 
-console.log("SideMenu Script was loaded.");
+console.log("Side Menu Script was loaded.");
