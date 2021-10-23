0c60aafb5d2ad399513883ffcc13e372cfa1b75c
Stake2 <kiko12345kiko@hotmail.com>
2021-10-23 16:58:57 -0300

    Remade 2019 website, made 2020 website, updated Watch History website to fix wrong movies number Added two new functions in "Redirect To Website Of User Language.js", Language_Item_Definer And "Check_Website_Link_By_Language", to use in the forEach of the new array of website names, work in progress To check all of the websites in the websites list file Added website icons for 2019 and 2020 websites

--- JavaScript/Function/Redirect To Website Of User Language.js
+++ JavaScript/Function/Redirect To Website Of User Language.js
@@ -1,10 +1,25 @@
+var website_link = "https://thestake2.netlify.app/";
+var user_language = navigator.website_language || navigator.userLanguage || navigator.language;
+
+var english_language = "en-US";
+var portuguese_language = "pt-BR" || "pt-PT";
+
+function Language_Item_Definer(english_text, portuguese_text, website_language) {
+	portuguese_language = ["pt-BR", "pt-PT"];
+	var language_text;
+
+	if (website_language == english_language) {
+		language_text = english_text;
+	}
+
+	if (portuguese_language.includes(website_language) == true) {
+		language_text = portuguese_text;
+	}
+
+	return language_text;
+}
+
 function Check_Website_Link() {
-	var website_link = "https://thestake2.netlify.app/";
-	var user_language = navigator.website_language || navigator.userLanguage || navigator.language;
-
-	var english_language = "en-US";
-	var portuguese_language = "pt-BR" || "pt-PT";
-
 	var title = document.getElementsByTagName("title")[0];
 	var website_title = title.innerHTML;
 
@@ -810,6 +825,97 @@ function Check_Website_Link() {
 			var res = choosen_website_url + user_language + "/";
 			window.location = res;
 			var thiss = that;
+		}
+	}
+
+	var current_variable_year = 2018;
+	var current_year = new Date().getFullYear();
+
+	var year_websites = [];
+
+	while (current_variable_year <= Number(current_year) - 1) {
+		year_websites.push(String(current_variable_year));
+
+		current_variable_year += 1;
+	}
+
+	var current_variable_year = 2018;
+	var current_year = new Date().getFullYear();
+
+	year_websites.forEach(Check_Website_Link_By_Language);
+}
+
+function Check_Website_Link_By_Language(item) {
+	var title = document.getElementsByTagName("title")[0];
+	var website_title = title.innerHTML;
+	var current_site_link = window.location;
+
+	var website_name = item;
+	var backup_website_name = "Years/" + website_name;
+	var additional_values = ["My", "Meu"];
+	var check = String(current_site_link).includes("no-redirect=true");
+
+	additional_value = additional_values[0] + " ";
+
+	website_name = additional_value + website_name;
+
+	if (website_title == website_name && check == false) {
+		var website_language = "General";
+
+		if (user_language == website_language) {
+			return;
+		}
+
+		if (user_language != website_language) {
+			var choosen_website_url = website_link + backup_website_name + "/";
+			var res = choosen_website_url + user_language.toLowerCase() + "/";
+			window.location = res;
+		}
+	}
+
+	if (website_title == website_name + " EN-US" && check == false) {
+		website_language = "en-US";
+
+		if (user_language == website_language) {
+			return;
+		}
+
+		if (user_language != website_language) {
+			var choosen_website_url = website_link + backup_website_name + "/";
+			var res = choosen_website_url + user_language.toLowerCase() + "/";
+			window.location = res;
+		}
+	}
+
+	additional_value = additional_values[1] + " ";
+
+	website_name = additional_value + website_name;
+
+	if (website_title == website_name + " PT-BR" && check == false) {
+		website_language = "pt-BR";
+
+		if (user_language == website_language) {
+			return;
+		}
+
+		if (user_language != website_language) {
+			var choosen_website_url = website_link + backup_website_name + "/";
+			var res = choosen_website_url + user_language.toLowerCase() + "/";
+			window.location = res;
+		}
+	}
+
+	if (website_title == website_name + " PT-PT" && check == false) {
+		website_language = "pt-PT";
+
+		if (user_language == website_language) {
+			return;
+		}
+
+		if (user_language != website_language) {
+			var choosen_website_url = website_link + backup_website_name + "/";
+			var res = choosen_website_url + user_language.toLowerCase() + "/";
+			window.location = res;
 		}
 	}
 }
