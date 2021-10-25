c5612fcc72e173850e8e5b1b46ce8893d06a3ae7
Stake2 <kiko12345kiko@hotmail.com>
2021-10-24 23:23:57 -0300

    Updated 2019 and 2020 websites to add 2021 button to years tab, added 2021 website

--- JavaScript/Function/Redirect To Website Of User Language.js
+++ JavaScript/Function/Redirect To Website Of User Language.js
@@ -230,48 +230,6 @@ function Check_Website_Link() {
 		}
 	}
 
-	if (website_title == "2019" && check == false) {
-		var website_language = "General";
-
-		if (user_language == website_language) {
-			return;
-		}
-
-		if (user_language != website_language) {
-			var choosen_website_url = website_link + "Years/" + "2019" + "/";
-			var res = choosen_website_url + user_language + "/";
-			window.location = res;
-		}
-	}
-
-	if (website_title == "2019 EN-US" && check == false) {
-		var website_language = "en-US";
-
-		if (user_language == website_language) {
-			return;
-		}
-
-		if (user_language != website_language) {
-			var choosen_website_url = website_link + "Years/" + "2019" + "/";
-			var res = choosen_website_url + user_language + "/";
-			window.location = res;
-		}
-	}
-
-	if (website_title == "2019 PT-BR" && check == false) {
-		var website_language = "pt-BR";
-
-		if (user_language == website_language) {
-			return;
-		}
-
-		if (user_language != website_language) {
-			var choosen_website_url = website_link + "Years/" + "2019" + "/";
-			var res = choosen_website_url + user_language + "/";
-			window.location = res;
-		}
-	}
-
 	if (website_title == "New World" && check == false) {
 		var website_language = "General";
 
@@ -833,7 +791,7 @@ function Check_Website_Link() {
 
 	var year_websites = [];
 
-	while (current_variable_year <= Number(current_year) - 1) {
+	while (current_variable_year <= Number(current_year)) {
 		year_websites.push(String(current_variable_year));
 
 		current_variable_year += 1;
